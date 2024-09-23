e8ca0aad802ad0aef73285567f5e303c802b3802
fish <shayancyler1612@gmail.com>
2024-09-23 19:55:23 +0330

    fix admin.py

--- notification/admin.py
+++ notification/admin.py
@@ -3,7 +3,7 @@ from notification.models import Notification
 
 
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('message', 'notification_type', 'is_sent', 'user', 'car')
+    list_display = ('message','is_sent', 'user', 'car')
 
 
 admin.site.register(Notification, NotificationAdmin)
